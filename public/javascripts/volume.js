c94cb3b430717fdceaabc76bf2677386b3aa70d0
Minjie Zhang <jie414341055@gmail.com>
2014-03-12 22:11:09 +0800

    start search function

--- public/javascripts/volume.js
+++ public/javascripts/volume.js
@@ -1,9 +1,25 @@
+function SearchProb() {
+	var info = document.getElementById('pid_or_title').value;
+	//alert(pid+"#"+user+"#"+result+"#"+lang);
+	if(!info) info = "";
+	var aform = document.creatElement('form');
+	aform.method = 'post';
+	aform.action='/ProbSearch';
+	var a=document.createElement('input');
+	a.type='hidden';
+	a.name='code';
+	a.value=info;
+	aform.appendChild(a);
+	document.body.appendChild(aform);
+	aform.submit();
+}
+/*
 (function(){
 	var $backToTopEle = $('.toTop'), $backToTopFun = function() {
 		alert('fuck');
 		var st = $(document).scrollTop(), winh = $(window).height();
 		(st > 200) ? $backToTopEle.fadeIn('slow') : $backToTopEle.fadeOut('slow');
-		//IE6下的定位
+
 		if (!window.XMLHttpRequest) {
 			$backToTopEle.css("top", st + winh - 166);
 		}
@@ -18,3 +34,4 @@
 		$("#catalog").slideToggle(600);
 	})
 })();
+*/
